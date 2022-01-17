c07cbc1b16db6b0654b619fc6d59f517fece89ea
tempakyousuke <tempakyousuke@gmail.com>
2022-01-18 03:12:06 +0900

    prepare mypage

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,24 @@
 <script>
 	import 'virtual:windi.css';
+	import { auth, db } from '$modules/firebase/firebase';
+	import { doc, getDoc } from 'firebase/firestore';
+	import { onAuthStateChanged } from 'firebase/auth';
+	import { user } from '$modules/store/store';
+	onAuthStateChanged(auth, async (firebaseUser) => {
+		if (firebaseUser) {
+			const userDoc = await getDoc(doc(db, 'users', firebaseUser.uid));
+			const userData = userDoc.data();
+			user.set({
+				uid: firebaseUser.uid,
+				name: userData.name
+			});
+		} else {
+			user.set({
+				uid: '',
+				name: ''
+			});
+		}
+	});
 </script>
 
 <div>
---
+++ src/routes/mypage/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { user } from '$modules/store/store';
+	import { goto } from '$app/navigation';
+	user.subscribe((user) => {
+		if (user.uid === '') {
+			goto('/');
+		}
+	});
+</script>
+
+<div>
+	<slot />
+</div>
