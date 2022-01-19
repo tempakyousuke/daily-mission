62e5c9f501d1a9d893138f018818de184f5ddda8
tempakyousuke <tempakyousuke@gmail.com>
2022-01-20 03:06:18 +0900

    get missions

--- src/routes/mypage/index.svelte
+++ src/routes/mypage/index.svelte
@@ -1,1 +1,17 @@
+<script lang="ts">
+	import { MissionModelFactory } from '$model/mission';
+	import type { MissionModel } from '$model/mission';
+	import { user } from '$modules/store/store';
+	let missions: MissionModel[] = [];
+
+	user.subscribe(async (user) => {
+		if (user.uid) {
+			missions = await MissionModelFactory.getFromUid(user.uid);
+		}
+	});
+</script>
+
 <a href="/mypage/mission/add">ミッション追加</a>
+{#each missions as mission}
+	{mission.title}
+{/each}
