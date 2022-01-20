86387912abed0ebbe77cfb44d0be50a1b09d9542
tempakyousuke <tempakyousuke@gmail.com>
2022-01-20 18:00:10 +0900

    place links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/mypage">マイページ</a>
+<a href="/signup">新規登録</a>
+<a href="/signin">ログイン</a>
